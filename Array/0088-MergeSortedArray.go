c7a38149457a6923347a76c893fa149d9c2df7ab
Kingslayer <2913045437@qq.com>
2023-10-22 21:43:22 +0800

    0088

---
+++ Array/0088-MergeSortedArray.go
@@ -0,0 +1,29 @@
+// Package main
+// -----------------------------------------------
+// @author  : Kingslayer
+// @school  : NJUST
+// @e-mail  : kingslayer5437@gmail.com
+// @time    : 2023/10/22 20:50
+// -----------------------------------------------
+package main
+
+import "sort"
+
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	if n == 0 {
+		return
+	}
+
+	if m == 0 {
+		for i := 0; i < n; i++ {
+			nums1[i] = nums2[i]
+		}
+		return
+	}
+
+	for i := 0; i < n; i++ {
+		nums1[m+i] = nums2[i]
+	}
+
+	sort.Ints(nums1)
+}

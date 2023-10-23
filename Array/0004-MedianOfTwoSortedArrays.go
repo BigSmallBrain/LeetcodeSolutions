7e80973824639a6d524bd4c02768a51ce233a9f7
Kingslayer <2913045437@qq.com>
2023-10-23 10:39:06 +0800

    0004

---
+++ Array/0004-MedianOfTwoSortedArrays.go
@@ -0,0 +1,27 @@
+// Package main
+// -----------------------------------------------
+// @author  : Kingslayer
+// @school  : NJUST
+// @e-mail  : kingslayer5437@gmail.com
+// @time    : 2023/10/23 10:30
+// -----------------------------------------------
+package main
+
+import "sort"
+
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	nums := append(nums1, nums2...)
+	sort.Ints(nums)
+
+	left, right := 0, len(nums)-1
+	for left < right {
+		left++
+		right--
+	}
+
+	if left == right {
+		return float64(nums[left])
+	} else {
+		return float64(nums[left]+nums[right]) / 2
+	}
+}

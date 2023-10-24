f1b945c0c88e40b74afd5ef5fbe5c83ae347bdbe
Kingslayer <2913045437@qq.com>
2023-10-24 16:44:48 +0800

    0027

---
+++ Array/0027-RemoveElement.go
@@ -0,0 +1,23 @@
+// Package main
+// -----------------------------------------------
+// @author  : Kingslayer
+// @school  : NJUST
+// @e-mail  : kingslayer5437@gmail.com
+// @time    : 2023/10/24 16:27
+// -----------------------------------------------
+package main
+
+func removeElement(nums []int, val int) int {
+	flag := len(nums)
+	for i := 0; i < flag; {
+		if nums[i] == val {
+			for j := i; j < flag-1; j++ {
+				nums[j] = nums[j+1]
+			}
+			flag--
+		} else {
+			i++
+		}
+	}
+	return flag
+}

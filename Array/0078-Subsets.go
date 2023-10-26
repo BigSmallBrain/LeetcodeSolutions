887f241798498e35609bf4fa67d6ff02ae2bc5f9
Kingslayer <2913045437@qq.com>
2023-10-26 11:32:51 +0800

    0078

---
+++ Array/0078-Subsets.go
@@ -0,0 +1,33 @@
+// Package main
+// -----------------------------------------------
+// @author  : Kingslayer
+// @school  : NJUST
+// @e-mail  : kingslayer5437@gmail.com
+// @time    : 2023/10/24 14:46
+// -----------------------------------------------
+package main
+
+func subsets(nums []int) [][]int {
+	n := len(nums)
+	res := make([][]int, 0, 1<<n)
+	path := make([]int, 0, n)
+
+	var dfs func(int2 int)
+	dfs = func(i int) {
+		if i == n {
+			res = append(res, append([]int{}, path...))
+			return
+		}
+
+		// 不选nums[i]
+		dfs(i + 1)
+
+		// 选nums[i]
+		path = append(path, nums[i])
+		dfs(i + 1)
+		path = path[:len(path)-1]
+	}
+
+	dfs(0)
+	return res
+}

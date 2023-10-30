9058283f496146d185a67bd573ea651b0c6ad0a4
Kingslayer <2913045437@qq.com>
2023-10-30 15:30:27 +0800

    0287

---
+++ Array/0287-FindTheDuplicateNumber.go
@@ -0,0 +1,22 @@
+// Package main
+// -----------------------------------------------
+// @author  : Kingslayer
+// @school  : NJUST
+// @e-mail  : kingslayer5437@gmail.com
+// @time    : 2023/10/30 10:50
+// -----------------------------------------------
+package main
+
+func findDuplicate(nums []int) int {
+	slow, fast := 0, 0
+	for slow, fast = nums[slow], nums[nums[fast]]; slow != fast; {
+		slow, fast = nums[slow], nums[nums[fast]]
+	}
+	slow = 0
+	for slow != fast {
+		slow = nums[slow]
+		fast = nums[fast]
+	}
+
+	return slow
+}

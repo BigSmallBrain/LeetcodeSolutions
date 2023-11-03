a3c8a85983d1592fb9491c0ebc2f474e688d396b
Kingslayer <2913045437@qq.com>
2023-11-03 22:09:10 +0800

    0901

---
+++ main/0901-OnlineStockSpan.go
@@ -0,0 +1,36 @@
+// Package main
+// -----------------------------------------------
+// @author  : Kingslayer
+// @school  : NJUST
+// @e-mail  : kingslayer5437@gmail.com
+// @time    : 2023/11/2 20:01
+// -----------------------------------------------
+package main
+
+// 单调栈
+
+type StockSpanner struct {
+	prices []int
+	stack  []int
+}
+
+func Constructor() StockSpanner {
+	return StockSpanner{}
+}
+
+func (s *StockSpanner) Next(price int) (res int) {
+
+	for len(s.stack) > 0 && price >= s.prices[s.stack[len(s.stack)-1]] {
+		s.stack = s.stack[:len(s.stack)-1]
+	}
+
+	if len(s.stack) == 0 {
+		res = len(s.prices) + 1
+	} else {
+		res = len(s.prices) - s.stack[len(s.stack)-1]
+	}
+	s.prices = append(s.prices, price)
+	s.stack = append(s.stack, len(s.prices)-1)
+
+	return
+}

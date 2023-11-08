246210fdf122756904f8c5b0b2c8fbc192bcc788
Kingslayer <2913045437@qq.com>
2023-11-08 15:16:34 +0800

    0344

---
+++ main/0344-ReverseString.go
@@ -0,0 +1,15 @@
+// Package main
+// -----------------------------------------------
+// @author  : Kingslayer
+// @school  : NJUST
+// @e-mail  : kingslayer5437@gmail.com
+// @time    : 2023/11/7 21:52
+// -----------------------------------------------
+package main
+
+// 双指针
+func reverseString(s []byte) {
+	for left, right := 0, len(s)-1; left < right; left, right = left+1, right-1 {
+		s[left], s[right] = s[right], s[left]
+	}
+}

65b1fee15ce2df374d7c8fbd116eef04d4c8c312
Kingslayer <2913045437@qq.com>
2023-11-15 21:21:48 +0800

    0561

---
+++ main/0561-ArrayPartition.go
@@ -0,0 +1,19 @@
+// Package main
+// -----------------------------------------------
+// @author  : Kingslayer
+// @school  : NJUST
+// @e-mail  : kingslayer5437@gmail.com
+// @time    : 2023/11/15 21:17
+// -----------------------------------------------
+package main
+
+import "sort"
+
+func arrayPairSum(nums []int) int {
+	sort.Ints(nums)
+	res := 0
+	for i := 0; i < len(nums); i += 2 {
+		res += nums[i]
+	}
+	return res
+}

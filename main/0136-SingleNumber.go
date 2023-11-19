54fc6a33856e51e656b81da1865e7875f4602f73
Kingslayer <2913045437@qq.com>
2023-11-19 16:23:15 +0800

    0136

---
+++ main/0136-SingleNumber.go
@@ -0,0 +1,18 @@
+// Package main
+// -----------------------------------------------
+// @author  : Kingslayer
+// @school  : NJUST
+// @e-mail  : kingslayer5437@gmail.com
+// @time    : 2023/11/18 21:50
+// -----------------------------------------------
+package main
+
+// 位运算
+
+func singleNumber(nums []int) int {
+	res := 0
+	for i := 0; i < len(nums); i++ {
+		res ^= nums[i]
+	}
+	return res
+}

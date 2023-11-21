5c884bdc07192022f819b5ba9a7a1c762d41862f
Kingslayer <2913045437@qq.com>
2023-11-21 11:12:22 +0800

    0704

---
+++ main/0704-BinarySearch.go
@@ -0,0 +1,25 @@
+// Package main
+// -----------------------------------------------
+// @author  : Kingslayer
+// @school  : NJUST
+// @e-mail  : kingslayer5437@gmail.com
+// @time    : 2023/11/21 11:07
+// -----------------------------------------------
+package main
+
+// 二分查找
+func search(nums []int, target int) int {
+	l, r := 0, len(nums)-1
+	for l <= r {
+		mid := (l + r) / 2
+		if nums[mid] == target {
+			return mid
+		}
+		if nums[mid] < target {
+			l = mid + 1
+		} else {
+			r = mid - 1
+		}
+	}
+	return -1
+}

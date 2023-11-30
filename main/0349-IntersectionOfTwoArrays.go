9ee3ae04b4de0a009601a9d0b598c72321d39c2a
Kingslayer <2913045437@qq.com>
2023-11-30 23:04:58 +0800

    0349 & 0350

---
+++ main/0349-IntersectionOfTwoArrays.go
@@ -0,0 +1,30 @@
+// Package main
+// -----------------------------------------------
+// @author  : Kingslayer
+// @school  : NJUST
+// @e-mail  : kingslayer5437@gmail.com
+// @time    : 2023/11/30 20:41
+// -----------------------------------------------
+package main
+
+import "sort"
+
+func intersection(nums1 []int, nums2 []int) []int {
+	sort.Ints(nums1)
+	sort.Ints(nums2)
+	n1, n2 := len(nums1), len(nums2)
+
+	res := make([]int, 0)
+	for i := 0; i < n1; i++ {
+		if i > 0 && nums1[i] == nums1[i-1] {
+			continue
+		}
+		index := sort.Search(n2-1, func(x int) bool {
+			return nums2[x] >= nums1[i]
+		})
+		if index < n2 && nums2[index] == nums1[i] {
+			res = append(res, nums1[i])
+		}
+	}
+	return res
+}
---
+++ main/0350-IntersectionOfTwoArraysII.go
@@ -0,0 +1,28 @@
+// Package main
+// -----------------------------------------------
+// @author  : Kingslayer
+// @school  : NJUST
+// @e-mail  : kingslayer5437@gmail.com
+// @time    : 2023/11/30 20:54
+// -----------------------------------------------
+package main
+
+import "sort"
+
+func intersect(nums1 []int, nums2 []int) []int {
+	sort.Ints(nums1)
+	sort.Ints(nums2)
+
+	n1, n2 := len(nums1), len(nums2)
+	res := make([]int, 0)
+	for i := 0; i < n1; i++ {
+		index := sort.Search(n2-1, func(x int) bool {
+			return nums2[x] >= nums1[i]
+		})
+		if index < n2 && nums2[index] == nums1[i] {
+			res = append(res, nums1[i])
+			nums2[index] = -1
+		}
+	}
+	return res
+}

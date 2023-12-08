8970cd04af065da994e03e9fbc4c934f621dafd7
Kingslayer <2913045437@qq.com>
2023-12-08 15:52:45 +0800

    0334

---
+++ main/0334-IncreasingTripletSubsequence.go
@@ -0,0 +1,35 @@
+// Package main
+// -----------------------------------------------
+// @author  : Kingslayer
+// @school  : NJUST
+// @e-mail  : kingslayer5437@gmail.com
+// @time    : 2023/12/7 16:48
+// -----------------------------------------------
+package main
+
+import "math"
+
+// 贪心算法
+
+func increasingTriplet(nums []int) bool {
+	n := len(nums)
+	if n < 3 {
+		return false
+	}
+
+	first, second := nums[0], math.MaxInt
+	for i := 1; i < n; i++ {
+		if nums[i] > second {
+			return true
+		} else if nums[i] > first {
+			second = nums[i]
+		} else {
+			first = nums[i]
+		}
+	}
+	return false
+}
+
+func main() {
+	increasingTriplet([]int{1, 5, 0, 4, 1, 3})
+}

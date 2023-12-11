50e6ede8f91c06b00be650badf3f903a63ace054
Kingslayer <2913045437@qq.com>
2023-12-11 19:38:11 +0800

    0987

---
+++ main/0978-LongestTurbulentSubarray.go
@@ -0,0 +1,42 @@
+// Package main
+// -----------------------------------------------
+// @author  : Kingslayer
+// @school  : NJUST
+// @e-mail  : kingslayer5437@gmail.com
+// @time    : 2023/12/8 16:00
+// -----------------------------------------------
+package main
+
+// 双指针
+
+func maxTurbulenceSize(arr []int) int {
+	n := len(arr)
+
+	res := 1
+	left, right := 0, 0
+	for right < n-1 {
+		if left == right {
+			if arr[left] == arr[left+1] {
+				left++
+			}
+			right++
+		} else {
+			if arr[right-1] < arr[right] && arr[right] > arr[right+1] {
+				right++
+			} else if arr[right-1] > arr[right] && arr[right] < arr[right+1] {
+				right++
+			} else {
+				left = right
+			}
+		}
+		if res < right-left+1 {
+			res = right - left + 1
+		}
+	}
+
+	return res
+}
+
+func main() {
+	maxTurbulenceSize([]int{9, 4, 2, 10, 7, 8, 8, 1, 9})
+}

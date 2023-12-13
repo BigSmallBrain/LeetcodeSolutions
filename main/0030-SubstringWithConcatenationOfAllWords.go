2b6387de6e65105fdb0dda2fdb696812aa0a9e30
Kingslayer <2913045437@qq.com>
2023-12-13 21:24:49 +0800

    0030

---
+++ main/0030-SubstringWithConcatenationOfAllWords.go
@@ -0,0 +1,57 @@
+// Package main
+// -----------------------------------------------
+// @author  : Kingslayer
+// @school  : NJUST
+// @e-mail  : kingslayer5437@gmail.com
+// @time    : 2023/11/5 17:04
+// -----------------------------------------------
+package main
+
+import (
+	"fmt"
+)
+
+// 滑动窗口 哈希表
+
+func findSubstring(s string, words []string) []int {
+	m, n := len(words), len(words[0])
+	length := m * n
+	if length > len(s) {
+		return []int{}
+	}
+
+	cmpMap := func(m1, m2 map[string]int) bool {
+		if len(m1) != len(m2) {
+			return false
+		}
+		for k, v1 := range m1 {
+			v2, ok := m2[k]
+			if !ok || v2 != v1 {
+				return false
+			}
+		}
+		return true
+	}
+
+	wordsCnt := make(map[string]int)
+	for i := 0; i < m; i++ {
+		wordsCnt[words[i]]++
+	}
+
+	res := make([]int, 0)
+	for l, r := 0, length; r <= len(s); l, r = l+1, r+1 {
+		tempCnt := make(map[string]int)
+		for i := l; i < r; i += n {
+			tempCnt[s[i:i+n]]++
+		}
+		if cmpMap(tempCnt, wordsCnt) {
+			res = append(res, l)
+		}
+	}
+	return res
+}
+
+func main() {
+	strs := []string{"word", "good", "best", "good"}
+	fmt.Println(findSubstring("wordgoodgoodgoodbestword", strs))
+}

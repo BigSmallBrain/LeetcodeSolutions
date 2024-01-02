8270934f063123dbdfe1cf8d1d1a035ad429d906
Kingslayer <2913045437@qq.com>
2024-01-02 18:42:04 +0800

    0242

---
+++ main/0242-ValidAnagram.go
@@ -0,0 +1,27 @@
+// Package main
+// -----------------------------------------------
+// @author  : Kingslayer
+// @school  : NJUST
+// @e-mail  : kingslayer5437@gmail.com
+// @time    : 2023/12/30 16:08
+// -----------------------------------------------
+package main
+
+// 哈希表
+
+func isAnagram(s, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+	cnt := map[rune]int{}
+	for _, ch := range s {
+		cnt[ch]++
+	}
+	for _, ch := range t {
+		cnt[ch]--
+		if cnt[ch] < 0 {
+			return false
+		}
+	}
+	return true
+}

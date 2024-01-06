daa4481ef9bd1e5266a9be8b390d817e1d20f243
Kingslayer <2913045437@qq.com>
2024-01-06 20:24:55 +0800

    0387

---
+++ main/0387-FirstUniqueCharacterInAString.go
@@ -0,0 +1,23 @@
+// Package main
+// -----------------------------------------------
+// @author  : Kingslayer
+// @school  : NJUST
+// @e-mail  : kingslayer5437@gmail.com
+// @time    : 2024/1/6 20:07
+// -----------------------------------------------
+package main
+
+// 哈希表
+
+func firstUniqChar(s string) int {
+	cnt := make([]int, 26)
+	for _, ch := range s {
+		cnt[ch-'a']++
+	}
+	for i, ch := range s {
+		if cnt[ch-'a'] == 1 {
+			return i
+		}
+	}
+	return -1
+}

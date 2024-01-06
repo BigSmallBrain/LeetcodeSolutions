860859981e3cd3d5cfff369663565ed153186d07
Kingslayer <2913045437@qq.com>
2024-01-06 21:04:13 +0800

    0041

---
+++ main/0041-FirstMissingPositive.go
@@ -0,0 +1,38 @@
+// Package main
+// -----------------------------------------------
+// @author  : Kingslayer
+// @school  : NJUST
+// @e-mail  : kingslayer5437@gmail.com
+// @time    : 2024/1/3 15:50
+// -----------------------------------------------
+package main
+
+// 哈希表
+
+func firstMissingPositive(nums []int) int {
+	n := len(nums)
+	abs := func(x int) int {
+		if x > 0 {
+			return x
+		}
+		return -x
+	}
+
+	for i := 0; i < n; i++ {
+		if nums[i] <= 0 {
+			nums[i] = n + 1
+		}
+	}
+	for i := 0; i < n; i++ {
+		num := abs(nums[i])
+		if num <= n {
+			nums[num-1] = -abs(nums[num-1])
+		}
+	}
+	for i := 0; i < n; i++ {
+		if nums[i] > 0 {
+			return i + 1
+		}
+	}
+	return n + 1
+}

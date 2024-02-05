a6628b56c59def4fdc29c354be75aff186b3f656
Kingslayer <2913045437@qq.com>
2024-02-05 20:33:01 +0800

    0292

---
+++ main/0292-NimGame.go
@@ -0,0 +1,14 @@
+// Package main
+// -----------------------------------------------
+// @author  : Kingslayer
+// @school  : NJUST
+// @e-mail  : kingslayer5437@gmail.com
+// @time    : 2024/2/4 19:28
+// -----------------------------------------------
+package main
+
+// 找规律
+
+func canWinNim(n int) bool {
+	return n%4 != 0
+}

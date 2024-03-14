36b20c01d53e9dcb1b74aff3c177d57513737ab1
Kingslayer <2913045437@qq.com>
2024-03-14 20:55:19 +0800

    0104

---
+++ main/0104-MaximumDepthOfBinaryTree.go
@@ -0,0 +1,15 @@
+// Package main
+// -----------------------------------------------
+// @author  : Kingslayer
+// @school  : NJUST
+// @e-mail  : kingslayer5437@gmail.com
+// @time    : 2024/3/14 20:39
+// -----------------------------------------------
+package main
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return max(maxDepth(root.Left), maxDepth(root.Right)) + 1
+}

be03275773805a7881ac200cc00263cee8a0b3d0
Kingslayer <2913045437@qq.com>
2024-03-17 20:21:34 +0800

    0226

---
+++ main/0226-InvertBinaryTree.go
@@ -0,0 +1,16 @@
+// Package main
+// -----------------------------------------------
+// @author  : Kingslayer
+// @school  : NJUST
+// @e-mail  : kingslayer5437@gmail.com
+// @time    : 2024/3/17 20:08
+// -----------------------------------------------
+package main
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	root.Left, root.Right = invertTree(root.Right), invertTree(root.Left)
+	return root
+}

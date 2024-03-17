66174bcf1e37c652f65a47ea240fffb66209531d
Kingslayer <2913045437@qq.com>
2024-03-17 19:30:56 +0800

    0112

---
+++ main/0112-PathSum.go
@@ -0,0 +1,18 @@
+// Package main
+// -----------------------------------------------
+// @author  : Kingslayer
+// @school  : NJUST
+// @e-mail  : kingslayer5437@gmail.com
+// @time    : 2024/3/17 19:12
+// -----------------------------------------------
+package main
+
+func hasPathSum(root *TreeNode, targetSum int) bool {
+	if root == nil {
+		return false
+	}
+	if root.Val == targetSum && root.Left == nil && root.Right == nil {
+		return true
+	}
+	return hasPathSum(root.Left, targetSum-root.Val) || hasPathSum(root.Right, targetSum-root.Val)
+}

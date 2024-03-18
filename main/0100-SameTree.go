4404ae59cf0b25aab4032947812d0c498a794ac5
Kingslayer <2913045437@qq.com>
2024-03-18 19:34:27 +0800

    0100

---
+++ main/0100-SameTree.go
@@ -0,0 +1,18 @@
+// Package main
+// -----------------------------------------------
+// @author  : Kingslayer
+// @school  : NJUST
+// @e-mail  : kingslayer5437@gmail.com
+// @time    : 2024/3/18 19:30
+// -----------------------------------------------
+package main
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+	if p == nil || q == nil {
+		return false
+	}
+	return p.Val == q.Val && isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+}

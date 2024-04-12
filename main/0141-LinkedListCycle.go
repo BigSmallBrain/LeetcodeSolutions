445146fc19fad7ebad43eac10d746db8bb3724ae
Kingslayer <2913045437@qq.com>
2024-04-12 19:55:41 +0800

    0141

---
+++ main/0141-LinkedListCycle.go
@@ -0,0 +1,22 @@
+// Package main
+// -----------------------------------------------
+// @author  : Kingslayer
+// @school  : NJUST
+// @e-mail  : kingslayer5437@gmail.com
+// @time    : 2024/4/12 19:36
+// -----------------------------------------------
+package main
+
+// 链表 快慢指针
+
+func hasCycle(head *ListNode) bool {
+	fast, slow := head, head
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+		if fast == slow {
+			return true
+		}
+	}
+	return false
+}

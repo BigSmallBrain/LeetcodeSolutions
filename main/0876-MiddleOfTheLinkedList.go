17f33dd7fa457cc9cb1487b2c9f4d14ed1e66731
Kingslayer <2913045437@qq.com>
2024-04-13 21:04:24 +0800

    0876

---
+++ main/0876-MiddleOfTheLinkedList.go
@@ -0,0 +1,19 @@
+// Package main
+// -----------------------------------------------
+// @author  : Kingslayer
+// @school  : NJUST
+// @e-mail  : kingslayer5437@gmail.com
+// @time    : 2024/4/13 20:55
+// -----------------------------------------------
+package main
+
+// 链表 快慢指针
+
+func middleNode(head *ListNode) *ListNode {
+	fast, slow := head, head
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+	}
+	return slow
+}

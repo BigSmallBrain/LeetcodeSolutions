b52de3319dce0cbea048d4c058e7914b9ae3b8bb
Kingslayer <2913045437@qq.com>
2024-04-19 12:56:09 +0800

    0222

---
+++ main/0222-CountCompleteTreeNodes.go
@@ -0,0 +1,17 @@
+// Package main
+// -----------------------------------------------
+// @author  : Kingslayer
+// @school  : NJUST
+// @e-mail  : kingslayer5437@gmail.com
+// @time    : 2024/4/19 12:50
+// -----------------------------------------------
+package main
+
+// 二叉树
+
+func countNodes(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return countNodes(root.Left) + countNodes(root.Right) + 1
+}

0c2c42d1914a78dbd54c13258d7df8a3c4029677
Kingslayer <2913045437@qq.com>
2024-06-24 18:34:39 +0800

    1143

---
+++ main/1143-LongestCommonSubsequence.go
@@ -0,0 +1,28 @@
+// Package main
+// -----------------------------------------------
+// @author  : Kingslayer
+// @school  : NJUST
+// @e-mail  : kingslayer5437@gmail.com
+// @time    : 2024/6/24 16:35
+// -----------------------------------------------
+package main
+
+// 动态规划
+
+func longestCommonSubsequence(text1 string, text2 string) (res int) {
+	m, n := len(text1), len(text2)
+	dp := make([][]int, m+1)
+	for i := range dp {
+		dp[i] = make([]int, n+1)
+	}
+	for i, c1 := range text1 {
+		for j, c2 := range text2 {
+			if c1 == c2 {
+				dp[i+1][j+1] = dp[i][j] + 1
+			} else {
+				dp[i+1][j+1] = max(dp[i][j+1], dp[i+1][j])
+			}
+		}
+	}
+	return dp[m][n]
+}
